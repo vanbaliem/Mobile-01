2dcf695d9ab19d2282cdd0f088a96713cb6e50e2
vanbaliem <your.github.liem07112004@gmail.com>
2025-08-01 17:29:43 +0700

    Initial commit of my Android project

--- app/src/main/java/com/example/hitcapp/LoginActivity.java
+++ app/src/main/java/com/example/hitcapp/LoginActivity.java
@@ -1,36 +1,38 @@
-package com.example.hitcapp; // ← Nhớ sửa lại đúng tên gói nếu khác
-
+package com.example.hitcapp;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        // Tránh tràn lên status bar
+        // Tránh giao diện tràn status bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Xử lý nút Back
-        Button btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
-            finish(); // đóng MainActivity để không quay lại bằng nút Back
         });
     }
 }
