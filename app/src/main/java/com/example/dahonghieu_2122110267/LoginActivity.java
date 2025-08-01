04bd70328a5b2bbff2573995f7d44857cb59342b
Da Hong Hieu <dahonghieu2122110267@gmail.com>
2025-08-01 14:25:03 +0700

    Initial commit

--- app/src/main/java/com/example/dahonghieu_2122110267/LoginActivity.java
+++ app/src/main/java/com/example/dahonghieu_2122110267/LoginActivity.java
@@ -11,23 +11,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
+            //connect button Login with
+        Button btnNext = findViewById(R.id.btnLogin);
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent it = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(it);
+            }
+        });
+        Button btnReister = findViewById(R.id.btnRegister);
+        btnReister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(),RegisterActivity.class);
                 startActivity(it);
             }
         });
--- app/src/main/java/com/example/dahonghieu_2122110267/RegisterActivity.java
+++ app/src/main/java/com/example/dahonghieu_2122110267/RegisterActivity.java
@@ -11,20 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(new View.OnClickListener() {
+        Button btnRegisterAccount = findViewById(R.id.btnRegisterAccount);
+        btnRegisterAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent it = new Intent(getApplicationContext(),LoginActivity.class);
